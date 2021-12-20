3a9fca7b3d8b558b35b6400caa7eb6b952d02c97
Victor Magarlamov <victor.magarlamov@gmail.com>
2021-12-19 19:51:54 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # boooa
+
+## Install
+
+npm i boooa
+
+## Usage
+
+```js
+import { swallow, spitOut } from "boooa";
+
+// Let's we have an array of similar objects...
+
+const data = [{
+  id: 1, title: "Item 1", status: 0,
+}, {
+  id: 2, title: "Item 2", status: 1,
+}];
+
+// Using the `swallow` method, we can stringify an array...
+
+const str = swallow(data);
+
+// Now it looks like {"data":[1,"Title 1",0,2,"Title 2",1],"schema":["id","title","status"]}
+// It allows us to reduce the size of array twice
+
+// Using the `spitOut` method, we can parse the string back into an array.
+
+const array = spitOut(str);
+```
--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-import "core-js/modules/es.object.to-string.js";
-import "core-js/modules/es.symbol.js";
-import "core-js/modules/es.symbol.description.js";
-import "core-js/modules/es.symbol.iterator.js";
-import "core-js/modules/es.array.iterator.js";
-import "core-js/modules/es.array.reduce.js";
-import "core-js/modules/es.string.iterator.js";
-import "core-js/modules/web.dom-collections.iterator.js";
-import "core-js/modules/es.array.from.js";
-import "core-js/modules/es.array.slice.js";
-import "core-js/modules/es.function.name.js";
-import "core-js/modules/es.regexp.exec.js";
-import "core-js/modules/es.regexp.test.js";
-
-export { default as swallow } from "./src/stringify";
-export { default as spitOut } from "./src/parse";
+export { default as swallow } from "./stringify";
+export { default as spitOut } from "./parse";
